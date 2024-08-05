6fb770483dbc722a132709ff58f137049ebb9397
Muhammad Osama Mushtaq <108954241+Osama-Mushtaq@users.noreply.github.com>
2024-08-05 10:18:14 +0500

    checking commit without using -m flag

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       child: ScreenUtilInit(
         builder: (context, child) => MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo',
+          title: 'Flutter Demo 2',
           theme: ThemeData(
             appBarTheme: const AppBarTheme(
               iconTheme: IconThemeData(color: AppColors.primaryText),
