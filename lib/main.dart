8deb54adde8203766f03d1c431c99ce6250baf88
Muhammad Osama Mushtaq <108954241+Osama-Mushtaq@users.noreply.github.com>
2024-08-05 10:21:55 +0500

    my third try of commiting now with -m flag

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       child: ScreenUtilInit(
         builder: (context, child) => MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo 2',
+          title: 'Flutter Demo 3',
           theme: ThemeData(
             appBarTheme: const AppBarTheme(
               iconTheme: IconThemeData(color: AppColors.primaryText),
