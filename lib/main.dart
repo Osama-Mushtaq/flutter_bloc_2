cfcd36aae06bb377aecee70f129abca01fe14795
Muhammad Osama Mushtaq <108954241+Osama-Mushtaq@users.noreply.github.com>
2024-08-05 10:40:09 +0500

    some more on gittry

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       child: ScreenUtilInit(
         builder: (context, child) => MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo 3',
+          title: 'Flutter Demo 3435435342324',
           theme: ThemeData(
             appBarTheme: const AppBarTheme(
               iconTheme: IconThemeData(color: AppColors.primaryText),
